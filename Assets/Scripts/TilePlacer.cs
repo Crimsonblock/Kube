e36fe8af40e2ac81934cf83547bce095d4694648
Crimsonblock <crimsonblock1991+github@gmail.com>
2022-11-03 14:25:42 +0100

    Added the tile placement and the basic tiles

--- Assets/Scripts/TilePlacer.cs
+++ Assets/Scripts/TilePlacer.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class TilePlacer : MonoBehaviour
 {
+    public colliderFace face;
 
     private char numLoops;
+    private const float tileOffset = 0.28f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +19,45 @@ public class TilePlacer : MonoBehaviour
     {
         if (numLoops > 10)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
         numLoops++;
     }
 
-    public colliderFace face;
+    
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Tile")
         {
             other.transform.SetParent(transform.parent, true);
+            other.transform.position = transform.position;
+            Vector3 otherPosition = other.transform.position;
+            switch (face)
+            {
+                case colliderFace.FRONT:
+                    other.transform.position = new Vector3(otherPosition.x, otherPosition.y, otherPosition.z + tileOffset);
+                    break;
+                case colliderFace.BACK:
+                    other.transform.position = new Vector3(otherPosition.x, otherPosition.y, otherPosition.z - tileOffset);
+                    break;
+                case colliderFace.LEFT:
+                    other.transform.position = new Vector3(otherPosition.x + tileOffset, otherPosition.y, otherPosition.z);
+                    break;
+                case colliderFace.RIGHT:
+                    other.transform.position = new Vector3(otherPosition.x - tileOffset, otherPosition.y, otherPosition.z);
+                    break;
+                case colliderFace.TOP:
+                    other.transform.position = new Vector3(otherPosition.x, otherPosition.y-tileOffset, otherPosition.z);
+                    break;
+                case colliderFace.BOTTOM:
+                    other.transform.position = new Vector3(otherPosition.x, otherPosition.y + tileOffset, otherPosition.z);
+                    break;
+                default:
+                    Debug.Log("Invalid face value");
+                    break;
+            };
+
+            Destroy(gameObject);
         }
     }
 }
