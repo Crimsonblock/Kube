851a7e0fae07fd7e6baff475160abaf083831953
Yonathan Irawan <yonathan.julian@gmail.com>
2022-11-04 13:11:28 +0100

    Rotate cube

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    public CubeOrientation orientation;
+
+    float gyro = -0f;
+    float accel = -0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        orientation.gyroscopeX = gyro;
+        orientation.accelerationX = accel;
     }
 }
