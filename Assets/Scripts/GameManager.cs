eb70bcb10cbfa4fdfe7ec7f18871a437e28ad2a6
Crimsonblock <crimsonblock1991+github@gmail.com>
2022-12-15 22:00:45 +0100

    disabled the data persistency to avoid

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
         bleManager = BleManager.getInstance(true);
         bleManager.startScan();
         //bleManager = null;
-        DontDestroyOnLoad(gameObject);
+        // DontDestroyOnLoad(gameObject);
     }
 
     ~GameManager()
