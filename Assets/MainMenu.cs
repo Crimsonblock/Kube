e3fc6369e6ed47464b63b210e91318b1cef04e94
SteveDePicker <80755832+SteveDePicker@users.noreply.github.com>
2022-12-21 21:39:49 +0100

    Added the main meny

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() { 
-    //loading screen
-        SceneManager.LoadScene("level 1")
+    public void PlayGame() {
+        //loading screen
+        SceneManager.LoadScene("level 1");
     }
     
     
